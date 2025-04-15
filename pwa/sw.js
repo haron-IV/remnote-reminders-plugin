4752f73237ac88c04632cd6dca741e5f780cd6db
haron-IV <bart-zen@protonmail.com>
2025-04-15 13:06:48 +0200

    fix pwa

--- pwa/sw.js
+++ pwa/sw.js
@@ -5,14 +5,14 @@ const urlsToCache = [
   '/icon.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+// self.addEventListener('install', event => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(cache => {
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
