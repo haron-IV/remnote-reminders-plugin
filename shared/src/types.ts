79adc16befb75f1042bb80669d6ef10e3a09bcdb
Norah C. IV <30108189+haron-IV@users.noreply.github.com>
2025-04-19 21:00:39 +0200

    redesigned redirection page (#21)

---
+++ shared/src/types.ts
@@ -0,0 +1,17 @@
+/**
+ * Used for storing all the reminders. This schema is also sended to the server
+ */
+export interface RemindersData {
+  chatId?: number
+  timestamp?: number
+  reminders: Reminder[]
+}
+
+export interface Reminder {
+  date?: string
+  time?: string
+  text?: string
+  remId: string
+  timestamp: number
+  deeplink: string
+}
