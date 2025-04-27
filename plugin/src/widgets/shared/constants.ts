0299d792fdd4a1cf470a29b84ff43e41f60b993f
Norah C. IV <30108189+haron-IV@users.noreply.github.com>
2025-04-27 18:01:25 +0200

    feat(#55): production build adjustmen

--- plugin/src/widgets/shared/constants.ts
+++ plugin/src/widgets/shared/constants.ts
@@ -13,3 +13,7 @@ export enum SlotCode {
 export const SEND_REMINDERS_TO_API_DEBOUNCE_MS = 2000
 export const DATE_FORMAT = 'dd/MM/yyyy'
 export const REMINDER_TEXT_CHARACTER_LIMIT = 390
+
+const DEV_API_URL = 'http://localhost:3000'
+const PROD_API_URL = 'http://188.68.232.55:3000'
+export const API_URL = PROD_API_URL // Just change this value if you wan to test it differently
